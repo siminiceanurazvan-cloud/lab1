32634589b91b7e86d2e29d3ed3e7e4ccec675a7c
siminiceanurazvan-cloud <siminiceanurazvan@gmail.com>
2026-03-17 19:52:23 +0200

    lab2:pb1+pb2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,65 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        Student s1=new Student(448, "Siminiceanu","Razvan","TI22/1");
-        System.out.println(s1);
+
+    public static boolean existaStudent(List<Student> lista, Student deCautat) {
+        for (Student s : lista) {
+            if (s.getNume().equals(deCautat.getNume()) && s.getPrenume().equals(deCautat.getPrenume()) && s.getFormatieDeStudiu().equals(deCautat.getFormatieDeStudiu())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void main(String[] args) {
+        //PROBLEMA 1:
+        /*List<Integer> x = new ArrayList<>();
+        List<Integer> y = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 5; i++)
+            x.add(rand.nextInt(11));
+        for (int i = 0; i < 7; i++)
+            y.add(rand.nextInt(11));
+
+        Collections.sort(x);
+        Collections.sort(y);
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+
+        List<Integer> XplusY = new ArrayList<>(x);
+        XplusY.addAll(y);
+        Collections.sort(XplusY);
+        System.out.println("XplusY: " + XplusY);
+
+        Set<Integer> zSet = new TreeSet<>(x);
+        zSet.retainAll(y);
+        System.out.println("zSet: " + zSet);
+
+        List<Integer> XminusY = new ArrayList<>(x);
+        XminusY.removeAll(y);
+        System.out.println("XminusY: " + XminusY);
+
+        int p = 4;
+        List<Integer> XplusYlimitedbyP = new ArrayList<>(XplusY);
+*/
+        //PROBLEMA 2
+        Student s1 = new Student(448, "Razvan", "Siminiceanu", "TI22/1");
+        Student s2 = new Student(460, "Andrei", "Straculencu", "TI22/1");
+        Student s3 = new Student(455, "Patrik", "Rucarean", "TI22/1");
+        List<Student> listaStudenti = new ArrayList<>(Arrays.asList(s1, s2, s3));
+
+        System.out.println("Lista de studenti:");
+        for (Student s : listaStudenti) {
+            System.out.println(s);
+        }
+
+        Student cautat1 = new Student(120, "Alis", "Popa", "TI21/2");
+        System.out.println("\nEste Alis Popa in lista? " + existaStudent(listaStudenti, cautat1));
+
+        Student cautat2 = new Student(112, "Maria", "Popa", "TI21/1");
+        System.out.println("Este Maria Popa in lista? " + existaStudent(listaStudenti, cautat2));
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,12 +3,18 @@ public class Student {
     private String nume;
     private String prenume;
     private String formatieDeStudiu;
+
     public Student(int numarMatricol, String nume, String prenume, String formatieDeStudiu) {
         this.numarMatricol = numarMatricol;
         this.nume = nume;
         this.prenume = prenume;
         this.formatieDeStudiu = formatieDeStudiu;
     }
+
+    public String getNume() { return nume; }
+    public String getPrenume() { return prenume; }
+    public String getFormatieDeStudiu() { return formatieDeStudiu; }
+
     @Override
     public String toString() {
         return "Studentul " + numarMatricol + ": " + nume + " " + prenume + " - " + formatieDeStudiu + ".";
